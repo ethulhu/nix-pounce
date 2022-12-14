059d755a7994ad8753f50e346312429fa2ff4fce
Ethel Morgan <eth@ethulhu.co.uk>
2022-12-14 17:28:02 +0000

    add litterbox package; add flake overlays, packages, checks

--- flake.nix
+++ flake.nix
@@ -12,12 +12,23 @@
       supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
 
+      derivations = pkgs: {
+        litterbox = pkgs.callPackage ./pkgs/litterbox { };
+      };
+
     in {
+      overlays.default = final: _prev: derivations final;
+
+      packages =
+        forAllSystems (system: derivations nixpkgs.legacyPackages.${system});
+
       devShells = forAllSystems (system:
         let pkgs = nixpkgs.legacyPackages.${system};
         in {
           default =
             pkgs.mkShell { packages = with pkgs; [ nixfmt pre-commit reuse ]; };
         });
+
+      checks = self.packages;
     };
 }
---
+++ pkgs/litterbox/default.nix
@@ -0,0 +1,28 @@
+# SPDX-FileCopyrightText: 2022 Ethel Morgan
+#
+# SPDX-License-Identifier: MIT
+
+{ fetchzip, lib, libressl, pkgconfig, sqlite, stdenv }:
+stdenv.mkDerivation rec {
+  pname = "litterbox";
+  version = "1.8";
+
+  src = fetchzip {
+    url =
+      "https://git.causal.agency/litterbox/snapshot/litterbox-${version}.tar.gz";
+    hash = "sha256-KD+Jcwsz7yxpZzF+yj5j4eSsAIFy4icUknAWFJzCVjs=";
+  };
+
+  buildInputs = [ libressl sqlite ];
+
+  nativeBuildInputs = [ pkgconfig ];
+
+  buildFlags = [ "all" ];
+
+  meta = with lib; {
+    homepage = "https://code.causal.agency/june/litterbox";
+    description = "IRC logger";
+    license = licenses.gpl3;
+    platforms = platforms.linux;
+  };
+}
